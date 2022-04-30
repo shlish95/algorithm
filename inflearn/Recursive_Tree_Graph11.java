37a21d1e792c8ae2e3610c015c179f5899ee3dcd
shlish95 <fhzptvh@gmail.com>
2022-04-30 23:01:48 +0900

    Recursive_Tree_Graph11_경로탐색(인접행렬: DFS)

---
+++ inflearn/Recursive_Tree_Graph11.java
@@ -0,0 +1,64 @@
+package inflearn.recursive_tree_graph;
+
+import java.util.Scanner;
+
+/* 11. 경로 탐색(인접행렬, DFS)
+방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수 출력
+
+첫 줄에는 정점의 수 N, 간선의 수 M
+그 다음부터 M 줄에 걸쳐 연결 정보가 주어짐(출발 정점  도착 정점)
+
+입력 예시
+5 9
+1 2
+1 3
+1 4
+2 1
+2 3
+2 5
+3 4
+4 2
+4 5
+
+출력 예시
+6
+ */
+public class Recursive_Tree_Graph11 {
+    static int N, M, answer = 0;                //정점 수 N, 간선 수 M, 경로의 가지수 answer 선언 및 초기화
+    static int[][] graph;                       //각 정점간의 관계를 표현할 graph 2차원 배열 선언,
+                                                //예를 들어 정점 1에서 정점 2로 가는 간선이 있으면 좌표(1, 2)에 1 대입
+    static int[] ch;                            //지나온 정점을 표시(check) 해줄 배열 선언
+
+    private void DFS(int v) {                   //출발 정점의 번호 v를 매개변수로 받는 메서드
+        if (v == N) {                           //v가 N, 즉 마지막 정점이라면
+            answer++;                           //answer 증가
+        } else {                                //그렇지 않다면
+            for (int i = 1; i <= N; i++) {      //v에서 출발해 다른 정점으로 가는 간선이 있는지 N개의 간선 확인
+                if (graph[v][i] == 1 &&         //graph에 (출발정점 도착정점)으로 되어있으므로 [v][i]가 1이면 간선이 존재
+                        ch[i] == 0) {           //그리고 ch[i]가 0, 즉 지나오지 않았다면
+                    ch[i] = 1;                  //ch의 i번째 인덱스에 1대입하여 지나온 것을 확인 시켜준 후,
+                    DFS(i);                     //도착 정점의 번호를 매개변수로 DFS메서드 실행(재귀)
+                    ch[i] = 0;                  //메서드 실행 후, 지나온 정점을 표시해 두었던 ch 배열에 0 대입해 주어 초기화
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        graph = new int[N + 1][N + 1];          //정점이 1부터 N까지이므로 N+1로 크기 지정, 0열과 0행은 사용하지 않음
+        ch = new int[N + 1];                    //정점이 1부터 N까지이므로 N+1로 크기 지정, 0번 인덱스는 사용하지 않음
+
+        for (int i = 0; i < M; i++) {           //0으로 초기화 되어있는 graph 배열에 1을 대입해 간선 표시
+            int a = sc.nextInt();               //출발 정점
+            int b = sc.nextInt();               //도착 정점
+            graph[a][b] = 1;
+        }
+
+        Recursive_Tree_Graph11 r = new Recursive_Tree_Graph11();
+        ch[1] = 1;                              //출발 정점이 1이기 때문에 ch의 1번 인덱스는 1 대입
+        r.DFS(1);                            //메서드에 출발 정점 번호인 1을 매개변수로 넣고 실행
+        System.out.println(answer);
+    }
+}
